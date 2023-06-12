f631b1891e9123fe654ff8f5f028ec12cb5ab5f9
OlegSuperBro <olegrakovic323@gmail.com>
2023-06-12 09:34:10 +0300

    trying to fix for older versions

--- pyosudb/__init__.py
+++ pyosudb/__init__.py
@@ -2,11 +2,6 @@ from pyosudb.osudb import parse_osudb
 from pyosudb.collectiondb import parse_collectiondb
 from pyosudb.scoresdb import parse_scoresdb
 
-try:
-    from importlib import metadata
-    __version__ = metadata.version(__package__)
-except ImportError:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution(__package__).version
+__version__ = "0.1.0"
 
 __all__ = ["parse_osudb", "parse_collectiondb", "parse_scoresdb"]
